7951b2568c7c4c050bdb5625d3839af78908ec6c
Alexey Demidov <ademido@gmail.com>
2023-04-28 21:47:25 +0000

    Remove files that were moved already

--- test_sort-program.py
+++
@@ -1,9 +0,0 @@
-# Test to validate program works properly
-def test_sort_string():
-    sorted_string = "122388797"
-    assert sorted_string == "122388797"
-
-
-# call the test
-test_sort_string()
-print("Test Passed")
